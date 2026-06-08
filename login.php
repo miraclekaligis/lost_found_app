5ff78f5c7b8216b9e658b6f50448735eb78b1cd0
Dede Kaligis <officialmiraclekaligis@gmail.com>
2026-06-08 11:59:56 +0800

    commit

--- login.php
+++ login.php
@@ -67,7 +67,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - LostTrack</title>
+    <title>Login</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body class="body-login">
